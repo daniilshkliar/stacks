fc9475f8bc957369879214b47df1cf40e9d89d75
daniilshkliar <daniilshkliar@yandex.ru>
2023-06-05 15:10:02 +0300

    fix: cache first strategy

--- src/sw.ts
+++ src/sw.ts
@@ -1,23 +1,63 @@
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { CacheFirst } from "workbox-strategies";
-import { pageCache, imageCache, staticResourceCache } from "workbox-recipes";
+import { pageCache } from "workbox-recipes";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { ExpirationPlugin } from "workbox-expiration";
 
 declare const self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
-
 cleanupOutdatedCaches();
 
-pageCache();
+const staticResourcesMatchCallback = ({ request }: any) =>
+  request.destination === "style" ||
+  request.destination === "script" ||
+  request.destination === "worker";
 
-staticResourceCache();
+const imagesMatchCallback = ({ request }: any) =>
+  request.destination === "image";
 
-imageCache();
+const fontsMatchCallback = ({ request }: any) => request.destination === "font";
 
 registerRoute(
-  ({ request }) => request.destination === "font",
+  staticResourcesMatchCallback,
   new CacheFirst({
-    cacheName: "fonts",
+    cacheName: "static-resources",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
   })
 );
+
+registerRoute(
+  imagesMatchCallback,
+  new CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 150,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  fontsMatchCallback,
+  new CacheFirst({
+    cacheName: "fonts",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+pageCache();
